419e96b219ffebcd76b318c13483d78e713c3a0d
TobyAdd <66429886+TobyAdd@users.noreply.github.com>
2022-06-06 10:53:40 +0300

    Sex Files
    ah....

---
+++ SexManager/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace SexManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonAboutSlave = new System.Windows.Forms.Button();
+            this.buttonKO = new System.Windows.Forms.Button();
+            this.buttonHeadshot = new System.Windows.Forms.Button();
+            this.sexView = new System.Windows.Forms.ListView();
+            this.SlavesName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SlavesNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sexMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.headshootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutSlaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upD0T3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sexMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Location = new System.Drawing.Point(401, 420);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Û̸͂̅̍́͑̒͂̿̓̎̏̀̕̚p҈҇̏̂̏̑̃͂̐́̓̐͐̅̚̚D̸͗̇̂̓̅̆̎̓̇̐͠0̶̛͑̂͊͂̀̿͗̀́̊̓̅T̷̀̐͐̆̿̄͗̀̾̔͑́̾͒̇̚͝" +
+    "3҉͋̎̈́͂͑̍̏̅̏̊̀̔̀̾͋̚͝";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAbout.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
+            this.buttonAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAbout.Location = new System.Drawing.Point(12, 420);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(124, 23);
+            this.buttonAbout.TabIndex = 10;
+            this.buttonAbout.Text = "Sex Manager";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // buttonAboutSlave
+            // 
+            this.buttonAboutSlave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAboutSlave.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.buttonAboutSlave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAboutSlave.Location = new System.Drawing.Point(482, 420);
+            this.buttonAboutSlave.Name = "buttonAboutSlave";
+            this.buttonAboutSlave.Size = new System.Drawing.Size(75, 23);
+            this.buttonAboutSlave.TabIndex = 9;
+            this.buttonAboutSlave.Text = "About Slave";
+            this.buttonAboutSlave.UseVisualStyleBackColor = true;
+            this.buttonAboutSlave.Click += new System.EventHandler(this.buttonAboutSlave_Click);
+            // 
+            // buttonKO
+            // 
+            this.buttonKO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonKO.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.buttonKO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKO.Location = new System.Drawing.Point(563, 420);
+            this.buttonKO.Name = "buttonKO";
+            this.buttonKO.Size = new System.Drawing.Size(75, 23);
+            this.buttonKO.TabIndex = 8;
+            this.buttonKO.Text = "K.O.";
+            this.buttonKO.UseVisualStyleBackColor = true;
+            this.buttonKO.Click += new System.EventHandler(this.buttonKO_Click);
+            // 
+            // buttonHeadshot
+            // 
+            this.buttonHeadshot.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHeadshot.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.buttonHeadshot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHeadshot.Location = new System.Drawing.Point(644, 420);
+            this.buttonHeadshot.Name = "buttonHeadshot";
+            this.buttonHeadshot.Size = new System.Drawing.Size(75, 23);
+            this.buttonHeadshot.TabIndex = 7;
+            this.buttonHeadshot.Text = "Headshot";
+            this.buttonHeadshot.UseVisualStyleBackColor = true;
+            this.buttonHeadshot.Click += new System.EventHandler(this.buttonHeadshot_Click);
+            // 
+            // sexView
+            // 
+            this.sexView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sexView.BackColor = System.Drawing.Color.DimGray;
+            this.sexView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SlavesName,
+            this.SlavesNumber});
+            this.sexView.ContextMenuStrip = this.sexMenuStrip;
+            this.sexView.ForeColor = System.Drawing.Color.White;
+            this.sexView.FullRowSelect = true;
+            this.sexView.HideSelection = false;
+            this.sexView.Location = new System.Drawing.Point(15, 12);
+            this.sexView.Name = "sexView";
+            this.sexView.Size = new System.Drawing.Size(704, 402);
+            this.sexView.TabIndex = 12;
+            this.sexView.UseCompatibleStateImageBehavior = false;
+            this.sexView.View = System.Windows.Forms.View.Details;
+            this.sexView.SelectedIndexChanged += new System.EventHandler(this.sexView_SelectedIndexChanged);
+            // 
+            // SlavesName
+            // 
+            this.SlavesName.Text = "Slaves Name";
+            this.SlavesName.Width = 150;
+            // 
+            // SlavesNumber
+            // 
+            this.SlavesNumber.Text = "Slaves Name ID";
+            this.SlavesNumber.Width = 92;
+            // 
+            // sexMenuStrip
+            // 
+            this.sexMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.headshootToolStripMenuItem,
+            this.kOToolStripMenuItem,
+            this.aboutSlaveToolStripMenuItem,
+            this.upD0T3ToolStripMenuItem});
+            this.sexMenuStrip.Name = "sexMenuStrip";
+            this.sexMenuStrip.Size = new System.Drawing.Size(138, 92);
+            // 
+            // headshootToolStripMenuItem
+            // 
+            this.headshootToolStripMenuItem.Name = "headshootToolStripMenuItem";
+            this.headshootToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.headshootToolStripMenuItem.Text = "Headshoot";
+            this.headshootToolStripMenuItem.Click += new System.EventHandler(this.buttonHeadshot_Click);
+            // 
+            // kOToolStripMenuItem
+            // 
+            this.kOToolStripMenuItem.Name = "kOToolStripMenuItem";
+            this.kOToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.kOToolStripMenuItem.Text = "K.O.";
+            this.kOToolStripMenuItem.Click += new System.EventHandler(this.buttonKO_Click);
+            // 
+            // aboutSlaveToolStripMenuItem
+            // 
+            this.aboutSlaveToolStripMenuItem.Name = "aboutSlaveToolStripMenuItem";
+            this.aboutSlaveToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.aboutSlaveToolStripMenuItem.Text = "About Slave";
+            this.aboutSlaveToolStripMenuItem.Click += new System.EventHandler(this.buttonAboutSlave_Click);
+            // 
+            // upD0T3ToolStripMenuItem
+            // 
+            this.upD0T3ToolStripMenuItem.Name = "upD0T3ToolStripMenuItem";
+            this.upD0T3ToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.upD0T3ToolStripMenuItem.Text = "Û̸͂̅̍́͑̒͂̿̓̎̏̀̕̚p҈҇̏̂̏̑̃͂̐́̓̐͐̅̚̚D̸͗̇̂̓̅̆̎̓̇̐͠0̶̛͑̂͊͂̀̿͗̀́̊̓̅T̷̀̐͐̆̿̄͗̀̾̔͑́̾͒̇̚͝" +
+    "3҉͋̎̈́͂͑̍̏̅̏̊̀̔̀̾͋̚͝";
+            this.upD0T3ToolStripMenuItem.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(731, 455);
+            this.Controls.Add(this.sexView);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonAboutSlave);
+            this.Controls.Add(this.buttonKO);
+            this.Controls.Add(this.buttonHeadshot);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(489, 345);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sex Manager";
+            this.sexMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonAboutSlave;
+        private System.Windows.Forms.Button buttonKO;
+        private System.Windows.Forms.Button buttonHeadshot;
+        private System.Windows.Forms.ListView sexView;
+        private System.Windows.Forms.ColumnHeader SlavesName;
+        private System.Windows.Forms.ColumnHeader SlavesNumber;
+        private System.Windows.Forms.ContextMenuStrip sexMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem headshootToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutSlaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upD0T3ToolStripMenuItem;
+    }
+}
+
---
+++ SexManager/MainForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SexManager
+{
+    public partial class MainForm : Form
+    {
+        List<int> suspendList = new List<int>();
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+        public MainForm()
+        {
+            InitializeComponent();
+            UpdateSlaves();
+
+        }
+
+        void UpdateSlaves()
+        {
+            sexView.Items.Clear();
+            foreach (Process list in Process.GetProcesses())
+            {
+                string[] row = { list.ProcessName + ".exe", list.Id.ToString() };
+                var listViewItem = new ListViewItem(row);
+                sexView.Items.Add(listViewItem);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateSlaves();
+        }
+
+        bool KillProcess(int pid)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+                proc.WaitForExit();
+                proc.Dispose();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        bool SuspendProcess(int pid)
+        {
+            try
+            {
+                var process = Process.GetProcessById(pid);
+
+                foreach (ProcessThread pT in process.Threads)
+                {
+                    IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                    if (pOpenThread == IntPtr.Zero)
+                    {
+                        continue;
+                    }
+
+                    SuspendThread(pOpenThread);
+
+                    CloseHandle(pOpenThread);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        bool ResumeProcess(int pid)
+        {
+            try
+            {
+                var process = Process.GetProcessById(pid);
+
+                if (process.ProcessName == string.Empty)
+                {
+                    return false;
+                }
+
+                foreach (ProcessThread pT in process.Threads)
+                {
+                    IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                    if (pOpenThread == IntPtr.Zero)
+                    {
+                        continue;
+                    }
+
+                    var suspendCount = 0;
+                    do
+                    {
+                        suspendCount = ResumeThread(pOpenThread);
+                    } while (suspendCount > 0);
+
+                    CloseHandle(pOpenThread);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void buttonHeadshot_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in sexView.SelectedItems)
+            {
+                if (suspendList.Contains(Int32.Parse(item.SubItems[1].Text)))
+                {
+                    suspendList.Remove(Int32.Parse(item.SubItems[1].Text));
+                }
+                KillProcess(Int32.Parse(item.SubItems[1].Text));
+            }
+            UpdateSlaves();
+        }
+
+        private void buttonKO_Click(object sender, EventArgs e)
+        {
+            if (buttonKO.Text == "K.O.")
+            {
+                foreach (ListViewItem item in sexView.SelectedItems)
+                {
+                    if (!suspendList.Contains(Int32.Parse(item.SubItems[1].Text)))
+                    {
+                        suspendList.Add(Int32.Parse(item.SubItems[1].Text));
+                        SuspendProcess(Int32.Parse(item.SubItems[1].Text));
+                    }
+                    buttonKO.Text = "Wake up!";
+                    kOToolStripMenuItem.Text = "Wake up!";
+                }
+            }
+            else
+            {
+                foreach (ListViewItem item in sexView.SelectedItems)
+                {
+                    if (suspendList.Contains(Int32.Parse(item.SubItems[1].Text)))
+                    {
+                        suspendList.Remove(Int32.Parse(item.SubItems[1].Text));
+                        ResumeProcess(Int32.Parse(item.SubItems[1].Text));
+                    }
+                    buttonKO.Text = "K.O.";
+                    kOToolStripMenuItem.Text = "K.O.";
+                }
+            }
+        }
+
+        private void sexView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool check = false;
+
+            foreach (ListViewItem item in sexView.SelectedItems)
+            {
+                if (suspendList.Contains(Int32.Parse(item.SubItems[1].Text)))
+                {
+                    check = true;
+                    break;
+                }
+            }
+
+            if (check)
+            {
+                buttonKO.Text = "Wake up!";
+                kOToolStripMenuItem.Text = "Wake up!";
+            }
+            else
+            {
+                buttonKO.Text = "K.O.";
+                kOToolStripMenuItem.Text = "K.O.";
+            }
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            List<string> about = new List<string>();
+            about.Add("Cum inside me! ahh");
+            about.Add("My developer is TobyAdd, I love him ~uwu");
+            about.Add("I am a fight girl who can kill any process! UwU");
+            about.Add("You are so cool >W<");
+            about.Add("I am your Sex Manager");
+            about.Add("I am coded in C# OwO");
+            about.Add("Hello! UwU");
+
+            MessageBox.Show(about.OrderBy(_ => Guid.NewGuid()).First(), "Sex Manager", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        string GetProcessNameByPid(int pid)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                return proc.ProcessName;
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+        string GetStartTime(int pid)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                return $"{proc.StartTime.ToLongDateString()} {proc.StartTime.ToLongTimeString()}";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+        string GetProcessPriority(int pid)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                return $"{proc.PriorityClass}({proc.BasePriority})";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+        string GetSessionID(int pid)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                return $"{proc.SessionId}";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+
+
+
+
+        private void buttonAboutSlave_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in sexView.SelectedItems)
+            {
+                Process proc = Process.GetProcessById(Int32.Parse(item.SubItems[1].Text));
+                if (MessageBox.Show($"Name: {GetProcessNameByPid(Int32.Parse(item.SubItems[1].Text))}\nPID: {item.SubItems[1].Text}\nStart Time: {GetStartTime(Int32.Parse(item.SubItems[1].Text))}\nProcess priority: {GetProcessPriority(Int32.Parse(item.SubItems[1].Text))}\nSession ID: {GetSessionID(Int32.Parse(item.SubItems[1].Text))}",
+                     $"About {GetProcessNameByPid(Int32.Parse(item.SubItems[1].Text))}", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
